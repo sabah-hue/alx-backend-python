54ec4a212626e3aad5ca491194fea7738e7da082
sabah-hue <sabah.abdelbaset@gmail.com>
2024-08-06 06:23:02 +0300

    debug

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+takes a string a and a string b as arguments 
+nd returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concat two args"""
+    return str1 + str2
