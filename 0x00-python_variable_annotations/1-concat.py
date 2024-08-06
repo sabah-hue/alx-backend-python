cbdc14c9b017fe7cf9ce3360b7c5e21ce3cc6cc1
sabah-hue <sabah.abdelbaset@gmail.com>
2024-08-06 06:20:49 +0300

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+takes a string a and a string b as arguments 
+nd returns a concatenated string
+"""
+
+
+def concat(a: str, b: str) -> str:
+    """concat two args"""
+    return a + b
