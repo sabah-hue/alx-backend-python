791864b8463216684e922ec5fccc70496798ca37
sabah-hue <sabah.abdelbaset@gmail.com>
2024-08-06 05:57:03 +0300

    output issue

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ wait random n times """
     tasks = [wait_random(max_delay) for _ in range(n)]
     tasks = asyncio.as_completed(tasks)
